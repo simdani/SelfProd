5ee30fd552ac56d34dc3992e0f638ac2db71e245
Simonas Daniliauskas <dranxman@gmail.com>
2018-08-23 18:32:57 +0300

    initial commit

---
+++ SelfProdApi/SelfProd/Entities/Models/Journal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfProd.Entities.Models
+{
+    public class Journal
+    {
+        public int JournalId { get; set; }
+        public string Entry { get; set; }
+        public DateTime Published { get; set; }
+    }
+}
